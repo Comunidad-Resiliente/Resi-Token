32e8a69dbdf0febe18e1ae3bb19b2083e857d36d
AlejoLovallo <alejoe.lovallo@gmail.com>
2023-05-14 17:49:09 -0300

    add config files

---
+++ config/sbt.ts
@@ -0,0 +1,26 @@
+export const SBTConfig = {
+  NAMES: {
+    MENTOR: {
+      NAME: 'MENTOR_SBT',
+      SYMBOL: 'MRESISBT'
+    },
+    TREASURY: {
+      NAME: 'TREASURY_SBT',
+      SYMBOL: 'TRESISBT'
+    },
+    PROJECT_BUILDER: {
+      NAME: 'PROJECT_BUILDER_SBT',
+      SYMBOL: 'PRESISBT'
+    },
+    RESI_BUILDER: {
+      NAME: 'RESI_BUILDER_SBT',
+      SYMBOL: 'RRESISBT'
+    }
+  },
+  31337: {
+    //hardhat network
+    SERIE_ID: 1,
+    CONTRACT_URI: 'localhost:3000'
+  },
+  80001: {}
+}
