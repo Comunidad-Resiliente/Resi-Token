23d2641352d79f8d92de8d695de4fe4b290cb1b7
AlejoLovallo <alejoe.lovallo@gmail.com>
2023-06-04 16:46:45 -0300

    feeat(tests): incrementing test coverage

--- test/constants.ts
+++ test/constants.ts
@@ -1,5 +1,6 @@
 import {keccak256, toUtf8Bytes} from 'ethers/lib/utils'
 import {BigNumber} from 'ethers'
+import {ethers} from 'hardhat'
 
 export const MINTER_ROLE = keccak256(toUtf8Bytes('MINTER_ROLE'))
 export const MENTOR_ROLE = keccak256(toUtf8Bytes('MENTOR_ROLE'))
@@ -7,9 +8,10 @@ export const TREASURY_ROLE = keccak256(toUtf8Bytes('TREASURY_ROLE'))
 export const PROJECT_BUILDER_ROLE = keccak256(toUtf8Bytes('PROJECT_BUILDER_ROLE'))
 export const RESI_BUILDER_ROLE = keccak256(toUtf8Bytes('RESI_BUILDER_ROLE'))
 
-export const PROJECT_ONE = keccak256(toUtf8Bytes('PROJECT ONE'))
-export const PROJECT_TWO = keccak256(toUtf8Bytes('PROJECT TWO'))
-export const PROJECT_THREE = keccak256(toUtf8Bytes('PROJECT THREE'))
+export const PROJECT_ONE = ethers.utils.formatBytes32String('PROJECT ONE')
+export const PROJECT_TWO = ethers.utils.formatBytes32String('PROJECT TWO')
+export const PROJECT_THREE = ethers.utils.formatBytes32String('PROJECT THREE')
+export const PROJECT_FOUR = ethers.utils.formatBytes32String('PROJECT FOUR')
 
 export interface ISerie {
   startDate: BigNumber
