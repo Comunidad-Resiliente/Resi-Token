eaaf1f431e0bec87881eac63ecad41c5d50c716b
AlejoLovallo <alejoe.lovallo@gmail.com>
2023-08-10 23:05:06 -0300

    feat(docs): update doc

--- contracts/interfaces/IResiRegistry.sol
+++ contracts/interfaces/IResiRegistry.sol
@@ -28,65 +28,65 @@ interface IResiRegistry {
         address vault
     ) external;
 
-    function addProject(bytes32 _name) external;
+    function addProject(bytes32 name) external;
 
     function addProjects(bytes32[] memory names) external;
 
-    function disableProject(bytes32 _name) external;
+    function disableProject(bytes32 name) external;
 
-    function increaseSerieSupply(uint256 _serieId, uint256 _amount) external;
+    function increaseSerieSupply(uint256 serieId, uint256 amount) external;
 
-    function decreaseSerieSupply(uint256 _serieId, uint256 _amount) external;
+    function decreaseSerieSupply(uint256 serieId, uint256 amount) external;
 
     function closeSerie() external;
 
     function activeSerie() external view returns (uint256 id);
 
-    function isValidProject(uint256 _serie, bytes32 _project) external view returns (bool);
+    function isValidProject(uint256 serie, bytes32 project) external view returns (bool);
 
-    function isValidProject(bytes32 _project) external view returns (bool);
+    function isValidProject(bytes32 project) external view returns (bool);
 
     function getSBTSerie() external view returns (address);
 
-    function getSerieState(uint256 _serieId) external view returns (bool, uint256);
+    function getSerieState(uint256 serieId) external view returns (bool, uint256);
 
-    function getSerieSupply(uint256 _serieId) external view returns (uint256);
+    function getSerieSupply(uint256 serieId) external view returns (uint256);
 
-    function getSBTSerie(uint256 _serieId) external view returns (address);
+    function getSBTSerie(uint256 serieId) external view returns (address);
 
     function getTreasuryVault() external view returns (address);
 
-    function withdrawFromVault(uint256 _serieId, uint256 _amount, address _to) external;
+    function withdrawFromVault(uint256 serieId, uint256 amount, address to) external;
 
     /// @notice Emitted when the Registry is initialized.
     event RegistryInitialized();
 
     /// @notice Emitted when Resi Token Contract is set on Contract
-    /// @param _resiToken Resi Token address
-    event ResiTokenSet(address indexed _resiToken);
+    /// @param resiToken Resi Token address
+    event ResiTokenSet(address indexed resiToken);
 
     /// @notice Emitted when Treasury Vault address is set on Contract
-    /// @param _treasuryVault Treasury Vault address
-    event TreasuryVaultSet(address indexed _treasuryVault);
+    /// @param treasuryVault Treasury Vault address
+    event TreasuryVaultSet(address indexed treasuryVault);
 
-    event SerieSBTSet(uint256 activeSerieId, address indexed _sbt);
+    event SerieSBTSet(uint256 activeSerieId, address indexed sbt);
 
     event SerieCreated(
-        uint256 _id,
-        uint256 _startDate,
-        uint256 _endDate,
-        uint256 _numberOfProjects,
-        uint256 _maxSupply,
-        address indexed _vault
+        uint256 id,
+        uint256 startDate,
+        uint256 endDate,
+        uint256 numberOfProjects,
+        uint256 maxSupply,
+        address indexed vault
     );
 
     event SerieSupplyUpdated(uint256 oldSupply, uint256 newSupply);
 
-    event SerieClosed(uint256 _id);
+    event SerieClosed(uint256 id);
 
-    event ProjectAdded(bytes32 _name, uint256 serieId);
+    event ProjectAdded(bytes32 name, uint256 serieId);
 
-    event ProjectDisabled(bytes32 _name);
+    event ProjectDisabled(bytes32 name);
 
-    event WithdrawFromVault(uint256 _serieId, uint256 _amount, address indexed _to);
+    event WithdrawFromVault(uint256 serieId, uint256 amount, address indexed to);
 }
